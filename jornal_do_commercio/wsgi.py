4f18b31c2f3c62c7f3c6e330134a69267b5968c1
Walter Maia <waltersantiagoneto@gmail.com>
2025-10-20 20:38:32 -0300

    Subindo o esqueleto do projeto e configurando o crispy

---
+++ jornal_do_commercio/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jornal_do_commercio project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jornal_do_commercio.settings')
+
+application = get_wsgi_application()
